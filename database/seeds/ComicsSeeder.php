28fc39eae269a94200441c8a264f9618ee8d2e7f
giacomo1235 <giacomo.passariello@gmail.com>
2022-05-09 17:18:08 +0200

    prova

--- database/seeds/ComicsSeeder.php
+++ database/seeds/ComicsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Comic;
 
 class ComicsSeeder extends Seeder
 {
@@ -11,7 +12,7 @@ class ComicsSeeder extends Seeder
      */
     public function run() {
 
-        return [
+        $arrComics = [
             [
                 "title" => "Action Comics #1000: The Deluxe Edition",
                 "description" => "The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! This hardcover includes all the stories from that issue, plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80 Years Of Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s new look, scripts for the stories, the original art from the lost story featuring art by Curt Swan and more! Plus: a complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2 from 1938!",
@@ -121,5 +122,12 @@ class ComicsSeeder extends Seeder
                 "type" => "graphic novel"
             ]
         ];
+
+        foreach ($arrComics as $comicData) {
+            $comic = new Comic();
+            $comic->fill($comicData);
+            $comic->save();
+        }
+
     }
 }
